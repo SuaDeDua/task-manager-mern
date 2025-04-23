4172158626616ca945e37885eee114756394acb4
SuaDeDua <sokvai123hn@gmail.com>
2025-04-23 08:08:08 +0700

    first commit: Setup, Connect databases, Error handle

---
+++ backend/src/index.ts
@@ -0,0 +1,55 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import session from "cookie-session";
+import { config } from "./config/app.config";
+import connectDatabases from "./config/databases.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { HTTPSTATUS } from "./config/http.config";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import { BadRequestException } from "./utils/appError";
+
+const app = express();
+const BASE_PATH = config.BASE_PATH;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    name: "session",
+    keys: [config.SESSION_SECRET],
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: config.NODE_ENV === "production",
+    httpOnly: true,
+    sameSite: "lax",
+  })
+);
+
+app.use(
+  cors({
+    origin: config.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get(
+  "/",
+  asyncHandler(async(req: Request, res: Response, next: NextFunction) => {
+    throw new BadRequestException("This is a bad request")
+    res.status(HTTPSTATUS.OK).json({
+      message: "Hello Subscribe to the channel & share",
+    });
+  })
+);
+
+
+app.use(errorHandler);
+
+app.listen(config.PORT, async () => {
+  console.log(
+    `Server is listening on port ${config.PORT} in ${config.NODE_ENV}`
+  );
+  await connectDatabases();
+});
