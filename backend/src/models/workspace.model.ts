8df2ceb935d3ccf0c3245bea3a2116b769b6251f
SuaDeDua <sokvai123hn@gmail.com>
2025-04-26 16:02:29 +0700

    add models

---
+++ backend/src/models/workspace.model.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { generateInviteCode } from "../utils/uuid";
+
+export interface WorkspaceDocument extends Document {
+  name: string;
+  description: string;
+  owner: mongoose.Types.ObjectId;
+  inviteCode: string;
+  createAt: string;
+  updateAt: string;
+}
+
+const workspaceSchema = new Schema<WorkspaceDocument>({
+  name: { type: String, required: true, trim: true },
+  description: { type: String, required: false },
+  owner: { 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User", // Reference to Use model (the workspace creator)
+    required: true,
+    },
+    inviteCode:{
+      type: String,
+      required: true,
+      unique: true,
+      default: generateInviteCode,
+    }
+},
+  {
+    timestamps: true,
+  }
+);
+
+workspaceSchema.methods.resetInviteCode = function(){
+  this.inviteCode = generateInviteCode()
+}
+
+const WorkspaceModel = mongoose.model<WorkspaceDocument>("Workspace", workspaceSchema)
+
+export default WorkspaceModel
