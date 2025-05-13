a93b9c30ca5a793a1b16c45ee0926de9c0a8fd4f
SuaDeDua <sokvai123hn@gmail.com>
2025-05-13 14:53:59 +0700

    Register Endpoint

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -1,6 +1,20 @@
-import { ErrorRequestHandler } from "express";
+import { ErrorRequestHandler, Response } from "express";
 import { HTTPSTATUS } from "../config/http.config";
 import { AppError } from "../utils/appError";
+import { ZodError } from "zod";
+import { ErrorCodeEnum } from "../enums/error-code.enum";
+
+const formatZodError = (res: Response, error: ZodError) => {
+  const errors = error?.issues?.map((err) => ({
+    field: err.path.join("."),
+    message: err.message,
+  }));
+  return res.status(HTTPSTATUS.BAD_REQUEST).json({
+    message: "Validation failed",
+    errors: errors,
+    errorCode: ErrorCodeEnum.VALIDATION_ERROR,
+  });
+};
 
 export const errorHandler: ErrorRequestHandler = (
   error,
@@ -16,11 +30,15 @@ export const errorHandler: ErrorRequestHandler = (
     });
   }
 
-  if (error instanceof AppError){
+  if (error instanceof ZodError) {
+    return formatZodError(res, error);
+  }
+
+  if (error instanceof AppError) {
     return res.status(error.statusCode).json({
       message: error.message,
-      error: error.errorCode
-    })
+      error: error.errorCode,
+    });
   }
 
   return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
