a84d2b7c93e091d76938f00d9e98ddf14a1e16dc
SuaDeDua <sokvai123hn@gmail.com>
2025-05-23 16:17:49 +0700

    done login and logout endpoint

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import passport from "passport";
 import { config } from "../config/app.config";
-import { googleLoginCallback, loginController, registerUserController } from "../controllers/auth.controller";
+import { googleLoginCallback, loginController, logOutController, registerUserController } from "../controllers/auth.controller";
 
 const failedUrl = `${config.GOOGLE_CALLBACK_URL}?status=failure`;
 
@@ -9,6 +9,8 @@ const authRoutes = Router();
 
 authRoutes.post("/register",  registerUserController)
 authRoutes.post("/login", loginController)
+
+authRoutes.post("/logout", logOutController)
 
 authRoutes.get(
   "/google",
