3e5dbf39f020b834466679104749935020b8d899
SuaDeDua <sokvai123hn@gmail.com>
2025-05-26 16:53:29 +0700

    Current User Endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,8 @@ import "./config/passport.config"
 import passport from "passport";
 import authRoutes from "./routes/auth.route";
 import connectDatabase from "./config/database.config";
+import userRoutes from "./routes/user.route";
+import isAuthenticated from "./middlewares/isAuthenticated.middleware";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -52,6 +54,7 @@ app.get(
 );
 
 app.use(`${BASE_PATH}/auth`, authRoutes)
+app.use(`${BASE_PATH}/user`,isAuthenticated, userRoutes)
 
 app.use(errorHandler);
 
